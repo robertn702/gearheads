d22bef7433f145ab954df399c36fcef84782a20c
Robert Niimi <Robert Niimi>
2015-01-30 23:13:00 -0800

    removed some template code

--- www/js/app.js
+++ www/js/app.js
@@ -34,35 +34,55 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     views: {
       'menuContent': {
         templateUrl: "templates/search.html"
+        // controller:
       }
     }
   })
-  .state('app.browse', {
-    url: "/browse",
+  .state('app.profile', {
+    url: "/profile",
     views: {
       'menuContent': {
-        templateUrl: "templates/browse.html"
+        templateUrl: "templates/profile.html"
+        // controller:
       }
     }
   })
-  .state('app.playlists', {
-    url: "/playlists",
+  .state('app.items', {
+    url: "/items",
     views: {
       'menuContent': {
-        templateUrl: "templates/playlists.html",
-        controller: 'PlaylistsCtrl'
+        templateUrl: "templates/items.html"
+        // controller:
       }
     }
   })
-  .state('app.single', {
-    url: "/playlists/:playlistId",
+  .state('app.friends', {
+    url: "/friends",
     views: {
       'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
+        templateUrl: "templates/friends.html"
+        // conroller:
       }
     }
-  });
+  })
+  // .state('app.playlists', {
+  //   url: "/playlists",
+  //   views: {
+  //     'menuContent': {
+  //       templateUrl: "templates/playlists.html",
+  //       controller: 'PlaylistsCtrl'
+  //     }
+  //   }
+  // })
+  // .state('app.single', {
+  //   url: "/playlists/:playlistId",
+  //   views: {
+  //     'menuContent': {
+  //       templateUrl: "templates/playlist.html",
+  //       controller: 'PlaylistCtrl'
+  //     }
+  //   }
+  // });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/profile');
 });
