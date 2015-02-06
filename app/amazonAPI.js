5b1ce7a23093e4eaeaa2913d5175737adf0bbc2c
Robert Niimi <Robert Niimi>
2015-02-05 19:00:46 -0800

    added basic amazon api backend set up

---
+++ app/amazonAPI.js
@@ -0,0 +1,15 @@
+// Library Docs: https://github.com/dmcquay/node-apac
+
+var util = require('util');
+var OperationHelper = require('apac').OperationHelper;
+
+require('dotenv').load();
+
+var opHelper = new OperationHelper({
+    awsId: process.env.AWS_ID,
+    awsSecret: process.env.AWS_SECRET,
+    assocId: process.env.ASSOC_ID
+    // xml2jsOptions: an extra, optional, parameter for if you want to pass additional options for the xml2js module. (see https://github.com/Leonidas-from-XIV/node-xml2js#options)
+});
+
+module.exports = opHelper;
--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,22 @@
 // load necessary models here
+var AWS = require('./amazonAPI')
 
 module.exports = function(app) {
-// server routes ===========================================================
   // app.get('/', function(req, res) {
   //   res.render(__dirname + '/index.html'); // load our client/index.html file
   // });
 
   // app.get('/business', function(req, res) {});
+
+  // API Details: http://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemSearch.html
+  app.get('/search', function(req, res) {
+    AWS.execute('ItemSearch',
+      {
+        'SearchIndex': 'MusicalInstruments',
+        'Keywords': 'harry potter'
+      }, function(err, results) {
+        console.log(results);
+      })
+  });
+
 }
